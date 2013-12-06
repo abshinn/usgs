8d95246a1c4360fa6338d2fdbbc5dcf3f93f2b7e
Adam Shinn <adam.b.shinn@gmail.com>
2013-12-06 11:49:28 -0800

    cleaned up APIquery class, added docstrings

--- getquakes.py
+++ getquakes.py
@@ -1,101 +1,119 @@
 #!/usr/bin/env python3
 
-# PURPOSE
-#   Provide simple python call to USGS earthquake API.
-#
-# DEFAULTS
-#    start - "startdate" in the USGS query; set to beginning of 2013
-#      end - "enddate" in the query; set to current time
-#   minmag - "minmagnitude" in the query; set to 2.5
-#  lat&lon - "latitude" and "longitude" in query; set to 37.44N 122.77W (San Francisco)
-#   radius - "maxradius" in the query; set to 200km
-#
-# USAGE
-#    usgsAPI(starttime = "2013-01-01", endtime = "",
-#            minmagnitude = "2.5",
-#            latitude = "37.77", longitude = "-122.44",
-#            minradiuskm = "0", maxradiuskm = "200",
-#            format = "geojson")
-#
-# MODIFICATION
-#   - created November 2013 by Adam Shinn
+import urllib.request, urllib.parse, time
+#import pdb # DEBUG
 
-import sys, urllib.request, urllib.parse, re, time
-#import pdb
+class APIquery(object):
+    """USGS Earthquake API Python3 Wrapper
+    
+    KEYWORD ARGUMENTS
+        params** -- any USGS earthquake query paramter, for a list and
+                    description of parameters, visit:
+                        http://comcat.cr.usgs.gov/fdsnws/event/1/
+                    for the USGS API web interface, go to:
+                        http://earthquake.usgs.gov/earthquakes/search/
+                        
+    DEFAULT BEHAVIOR
+        If query format is indicated as csv or text, a text file is
+        written to disk, otherwise the result is returned with the
+        call to usgs.APIquery()
 
-parameters = { "starttime": "",
-                 "endtime": "",
-             "updateafter": "",
-           # rectangular box
-             "minlatitude": "", 
-             "maxlatitude": "",
-            "minlongitude": "",
-            "maxlongitude": "",
-           # circle
-                "latitude": "",
-               "longitude": "",
-               "minradius": "",
-             "minradiuskm": "",
-               "maxradius": "",
-             "maxradiuskm": "",
-           # other
-                "mindepth": "",
-                "maxdepth": "",
-            "minmagnitude": "",
-            "maxmagnitude": "",
-       "includeallorigins": "",
-    "includeallmagnitudes": "",
-         "includearrivals": "",
-           "includedelete": "",
-                 "eventid": "",
-                   "limit": "",
-                  "offset": "",
-                 "orderby": "", #time, time-asc, magnitude, magnitude-asc
-                 "catalog": "",
-             "contributor": "",
-           # extensions
-                  "format": "", #quakeml, csv, geojson, kml, xml, text
-               "eventtype": "", #earthquake will limit non-earthquake events
-            "reviewstatus": "",
-                  "minmmi": "",
-                  "maxmmi": "",
-                  "mincdi": "",
-                  "maxcdi": "",
-                 "minfelt": "",
-              "alertlevel": "",
-                  "mingap": "",
-                  "maxgap": "",
-                  "maxsig": "",
-             "producttype": ""}
+    USAGE
+        # obtain earthquake events surrounding San Francisco (within 
+        # 200 km) since 2013, minimum magnitude of 2.5, in geojson format
+        usgs.APIquery(starttime = "2013-01-01", endtime = "",
+                      minmagnitude = "2.5",
+                      latitude = "37.77", longitude = "-122.44",
+                      minradiuskm = "0", maxradiuskm = "200",
+                      format = "geojson")
 
-class usgsAPI(object):
+    MODIFICATION
+        Created by Adam Shinn, November 2013
+    """
+
+    parameters = { "starttime": "",
+                     "endtime": "",
+                 "updateafter": "",
+               # rectangular box
+                 "minlatitude": "", 
+                 "maxlatitude": "",
+                "minlongitude": "",
+                "maxlongitude": "",
+               # circle
+                    "latitude": "",
+                   "longitude": "",
+                   "minradius": "",
+                 "minradiuskm": "",
+                   "maxradius": "",
+                 "maxradiuskm": "",
+               # other
+                    "mindepth": "",
+                    "maxdepth": "",
+                "minmagnitude": "",
+                "maxmagnitude": "",
+           "includeallorigins": "",
+        "includeallmagnitudes": "",
+             "includearrivals": "",
+               "includedelete": "",
+                     "eventid": "",
+                       "limit": "",
+                      "offset": "",
+                     "orderby": "", #time, time-asc, magnitude, magnitude-asc
+                     "catalog": "",
+                 "contributor": "",
+               # extensions
+                      "format": "", #quakeml, csv, geojson, kml, xml, text
+                   "eventtype": "", #earthquake will limit non-earthquake events
+                "reviewstatus": "",
+                      "minmmi": "",
+                      "maxmmi": "",
+                      "mincdi": "",
+                      "maxcdi": "",
+                     "minfelt": "",
+                  "alertlevel": "",
+                      "mingap": "",
+                      "maxgap": "",
+                      "maxsig": "",
+                 "producttype": ""}
+
     def __init__(self, **params):
-        self.parameters = parameters
         for param in params.keys():
             if param in self.parameters.keys():
                 self.parameters[param] = params[param]
             else:
                 raise KeyError(param)
+        self.result = self.query()
+        if self.parameters["format"] == "csv" or self.parameters["format"] == "text":
+            self.writeResult()
+        else:
+            self.returnResult()
 
-        # USGS API Query
-        # The documentation for this API can be found at:
-        #   http://comcat.cr.usgs.gov/fdsnws/event/1/
+    def query(self):
+        """query USGS API at: http://comcat.cr.usgs.gov/fdsnws/event/1/query?"""
         url = "http://comcat.cr.usgs.gov/fdsnws/event/1/query?{}&".format(
                 urllib.parse.urlencode(self.parameters,safe = "+:") )
-        
         print("Querying USGS with: {}".format(url))
         with urllib.request.urlopen(url) as usgs:
             response = usgs.read() # binary string
+        return response
 
-        # write the url response to a csv file
-        filename = "quake_last3decades.{}".format(self.parameters['format'])
+    def returnResult(self):
+        """return result from usgs.APIquery() call"""
+        print("Returned in {} format, string of length {}".format(self.parameters["format"], len(self.result)))
+        return self.result
+
+    def writeResult(self):
+        """write result from usgs.APIquery() call to text file"""
+        filename = "usgsQuery_{}.{}".format(
+                time.strftime("%Y-%m-%d_%H%M",time.localtime()), self.parameters["format"] )
         print("Writing results to: {}".format(filename))
         with open(filename, "wb") as btxt:
-            btxt.write(response)
+            btxt.write(self.result)
 
 if __name__ == '__main__':
-    usgsAPI(starttime = "2013-01-01", endtime = "",
-            minmagnitude = "2.5",
-            latitude = "37.77", longitude = "-122.44",
-            minradiuskm = "0", maxradiuskm = "200",
-            format = "geojson")
+    print("USGS APIquery Example")
+    APIquery(starttime = "2013-01-01", endtime = "",
+             minmagnitude = "2.5",
+             latitude = "37.77", longitude = "-122.44",
+             minradiuskm = "0", maxradiuskm = "200",
+             format = "geojson")
