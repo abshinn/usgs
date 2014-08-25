347fca38a5b2896e96cc10b0dd75162746acef0a
Adam Shinn <adam.b.shinn@gmail.com>
2014-08-24 20:58:57 -0700

    update data set to include the August 2014 sixer

--- exploration/getquakes.py
+++ exploration/getquakes.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     import usgs
 
     # obtain data for the greater San Francisco area, 1983 through 2012
-    usgs.APIquery(starttime = "1984-08-25", endtime = "2013-08-25",
-                  minmagnitude = "0.1",
+    usgs.APIquery(starttime = "1984-08-25", endtime = "2014-08-25T02:17:26",
+                  minmagnitude = "1.5",
                   latitude = "37.77", longitude = "-122.44",
                   minradiuskm = "0", maxradiuskm = "200",
                   reviewstatus = "reviewed",
@@ -18,8 +18,8 @@ if __name__ == "__main__":
                   format = "csv")
 
     # obtain data for the greater Los Angeles area, 1983 through 2012
-    usgs.APIquery(starttime = "1984-08-25", endtime = "2013-08-25",
-                  minmagnitude = "0.1",
+    usgs.APIquery(starttime = "1984-08-25", endtime = "2014-08-25T02:17:26",
+                  minmagnitude = "1.5",
                   latitude = "34.05", longitude = "-118.26",
                   minradiuskm = "0", maxradiuskm = "200",
                   reviewstatus = "reviewed",
