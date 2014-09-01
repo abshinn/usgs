88924c0c82ecc3d052317007896a23a1b2cfab2f
Adam Shinn <adam.b.shinn@gmail.com>
2014-09-01 11:08:18 -0700

    add verbose, print url, option

--- code/quakes.sh
+++ code/quakes.sh
@@ -5,12 +5,14 @@
 howlong='day'
 sortmag=false
 helpmsg=false
+verbose=false
 
-while getopts 'Hsh' flag; do
+while getopts 'Hshv' flag; do
   case "${flag}" in
     H) howlong='hour' ;;
     s) sortmag=true   ;;
     h) helpmsg=true   ;;
+    v) verbose=true   ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
@@ -23,11 +25,16 @@ if $helpmsg; then
     echo      -h    display this help message
     echo      -H    only show earthquakes in the past Hour, default is Day
     echo      -s    sort by magnitude, header removed 
+    echo      -v    print feed url
     exit
 fi
 
 url='http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_'
 ext='.csv'
+
+if $verbose; then
+    echo $url$howlong$ext
+fi
 
 if $sortmag; 
     then curl -s $url$howlong$ext | sed 's/, / - /g' | cut -d, -f 1-5,14 | column -s, -t | tail +2 | sort -n -k 5,5 -r
